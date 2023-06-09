fe44d6b3e388976431f196c8dce0889a06f12014
ibmorr <jakubekk5@gmail.com>
2023-06-09 14:16:55 +0200

    Small changes

--- ClinicApp/src/main/java/pl/edu/ur/pz/clinicapp/views/VisitsDetailsView.java
+++ ClinicApp/src/main/java/pl/edu/ur/pz/clinicapp/views/VisitsDetailsView.java
@@ -2,10 +2,6 @@ package pl.edu.ur.pz.clinicapp.views;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -22,10 +18,12 @@ import pl.edu.ur.pz.clinicapp.utils.ChildControllerBase;
 
 import java.net.URL;
 import java.sql.Timestamp;
-import java.time.*;
-import java.time.chrono.ChronoZonedDateTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 import static pl.edu.ur.pz.clinicapp.utils.OtherUtils.nullCoalesce;
 
@@ -77,8 +75,6 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
     private Mode currMode;
     private Appointment appointment;
 
-    private final ObservableList<Integer> hours = FXCollections.observableArrayList();
-
 
     /**
      * Displays alert about unsaved changes and returns whether user wants to discard them or not.
@@ -121,16 +117,17 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
     /**
      * Checks current window mode and user's identity and accordingly removes forbidden activities (edit and deletion
      * for non-creators of the referral or deletion if mode is set to CREATE).
-     * @param context contains current mode of window and appointment which is currently displaying/editing.
+     * @param context contains current mode of window and appointment which is currently displaying/editing
+     *                or User which is creating new appointment.
      */
     @Override
     public void populate(Object... context) {
         User.Role role = ClinicApplication.requireUser().getRole();
         currMode = (Mode) context[0];
 
-
         if (currMode == Mode.DETAILS) {
-            populateDetails((Appointment) context[1], role);
+            appointment = (Appointment) context[1];
+            populateDetails(appointment, role);
         }
         else if (currMode == Mode.CREATE) {
             populateCreate();
@@ -151,9 +148,8 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
         patientCombo.setValue(appointment.getPatient());
         doctorCombo.getItems().add(appointment.getDoctor());
         doctorCombo.setValue(appointment.getDoctor());
+        datePicker.setDisable(true);
 
-        LocalDateTime hourLDT = appointment.getDate().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        int hour = hourLDT.getHour()*60 + hourLDT.getMinute();
         notesTextField.setEditable(false);
         notesTextField.setText(appointment.getNotes());
         // in case the referral was edited while app is running
@@ -207,91 +203,6 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
 
     }
 
-//    /** Function refreshes hours of selected doctor to match his timetable and exclude hours that are occupied
-//     * @param newValue date for which hours are being refreshed
-//     *                 if null, current date is used
-//     *                 if not null, hours for this date are refreshed
-//     *                 (so that doctor can't be assigned to two appointments at the same time)
-//     * @param localAppointment appointment which is currently displaying/editing
-//     *                         if null, no hours are excluded
-//     *                         if not null, hours of this appointment are excluded
-//     *                         (so that doctor can't be assigned to two appointments at the same time)
-//     * **/
-//    private void refreshHours(LocalDate newValue, Appointment localAppointment) {
-//        if(newValue == null) newValue = LocalDate.now();
-//        final int[] dayHours = {-1, -1};
-//        hours.clear();
-//        List<Timetable> timetables = doctorCombo.getValue().asUser().getTimetables();
-//        for (Timetable timetable : timetables) {
-//            if (timetable.getEffectiveDate().isEqual(ChronoZonedDateTime.from(newValue))) {
-//                for (Timetable.Entry entry : timetable.getEntries()) {
-//                    dayHours[0] = entry.getStartMinute();
-//                    dayHours[1] = entry.getEndMinute();
-//                    ArrayList<Integer> occupiedHours = getOccupiedHours(dayHours[0], localAppointment);
-//                    while (dayHours[0] != -1 && dayHours[0] <= dayHours[1]) {
-//                        if(localAppointment != null) {
-//                            if(occupiedHours.contains(dayHours[0]) &&
-//                                    dayHours[0] != localAppointment.getDate().atZone(ZoneOffset.UTC).getHour() * 60
-//                                            + localAppointment.getDate().atZone(ZoneOffset.UTC).getMinute()) {
-//                                dayHours[0] += entry.getDurationMinutes();
-//                                continue;
-//                            }
-//                        } else {
-//                            if(occupiedHours.contains(dayHours[0])) {
-//                                dayHours[0] += entry.getDurationMinutes();
-//                                continue;
-//                            }
-//                        }
-//                        hours.add(dayHours[0]);
-//                        dayHours[0] += entry.getDurationMinutes();
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-    private void getFirstDate() {
-        LocalDate firstDate = LocalDate.now();
-        Schedule schedule = Schedule.of(doctorCombo.getValue().asUser());
-    }
-
-    /**
-     * Function returns list of hours that are occupied by appointments of selected doctor
-     * @param startHour hour from which hours are being checked
-     * @param localAppointment appointment which is currently displaying/editing
-     *                         if null, no hours are excluded
-     *                         if not null, hours of this appointment are excluded
-     *                         (so that doctor can't be assigned to two appointments at the same time)
-     * @return list of occupied hours
-     */
-    private ArrayList<Integer> getOccupiedHours(Integer startHour, Appointment localAppointment) {
-        ArrayList<Integer> occupiedHours = new ArrayList<>();
-        if(startHour != -1){
-            if (localAppointment != null) {
-                List<Appointment> appointments = ClinicApplication.getEntityManager()
-                        .createNamedQuery("allAppointmentsForDoctor", Appointment.class)
-                        .setParameter("doctor", localAppointment.getDoctor())
-                        .setParameter("id", localAppointment.getId())
-                        .getResultList();
-                for (Appointment local : appointments) {
-                    occupiedHours.add(local.getDate().atZone(ZoneOffset.UTC).getHour() * 60 +
-                            local.getDate().atZone(ZoneOffset.UTC).getMinute());
-                }
-            } else {
-                List<Appointment> appointments = ClinicApplication.getEntityManager()
-                        .createNamedQuery("allAppointmentsForDoctor", Appointment.class)
-                        .setParameter("doctor", doctorCombo.getValue())
-                        .setParameter("id", -1)
-                        .getResultList();
-                for (Appointment local : appointments) {
-                    occupiedHours.add(local.getDate().atZone(ZoneOffset.UTC).getHour() * 60 +
-                            local.getDate().atZone(ZoneOffset.UTC).getMinute());
-                }
-            }
-        }
-        return occupiedHours;
-    }
-
     /**
      * Sets values of table cells.
      */
@@ -302,6 +213,7 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
     /** Creating transaction and execute queries depending on type of action **/
     @FXML
     public void editSave() {
+        datePicker.setDisable(false);
         try {
             if (currMode == Mode.DETAILS) {
                 editSaveDetails();
@@ -335,6 +247,7 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
                 editQuery.executeUpdate();
                 transaction.commit();
                 ClinicApplication.getEntityManager().refresh(appointment);
+                datePicker.setDisable(true);
             }
         }
     }
@@ -370,7 +283,6 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
             alert.setHeaderText("Pomyślnie dodano wizytę.");
             alert.showAndWait();
             this.getParentController().goBack();
-            return;
         }
     }
     /**
@@ -449,40 +361,10 @@ public class VisitsDetailsView extends ChildControllerBase<MainWindowController>
         };
         doctorCombo.setButtonCell(cellDoctorFactory.call(null));
         doctorCombo.setCellFactory(cellDoctorFactory);
-        Callback<ListView<Integer>, ListCell<Integer>> cellHourFactory = new Callback<>() {
-
-            @Override
-            public ListCell<Integer> call(ListView<Integer> l) {
-                return new ListCell<>() {
-
-                    @Override
-                    protected void updateItem(Integer item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setGraphic(null);
-                        } else {
-                            setText(minutesToHour(item));
-                        }
-                    }
-                };
-            }
-        };
         pickedDate.setEditable(false);
     }
 
     public enum Mode {DETAILS, CREATE}
-
-    /** Converts hour(integer) to text form used in details view
-     * @param minutes - minutes to convert
-     * @return String in format HH:MM
-     * **/
-    private String minutesToHour(int minutes) {
-        int hours = (int) Math.floor((double) minutes /60);
-        int remainingMinutes = minutes - hours*60;
-        if (remainingMinutes == 0)
-            return hours + ":" + remainingMinutes + "0";
-        return hours + ":" + remainingMinutes;
-    }
 
 
     /** Function for picking date and time of appointment **/
